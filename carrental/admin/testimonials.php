f5c9b17670e10464657b717f17dfa23868647ab7
piamjeku <shqiperimjeku1@gmail.com>
2025-05-25 22:53:16 +0200

    Add testimonials page layout

--- carrental/admin/testimonials.php
+++ carrental/admin/testimonials.php
@@ -29,3 +29,28 @@ else {
         $msg="Testimonial Successfully Active";
     }
 ?>
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    ...
+    <title>Car Rental Portal | Admin Manage testimonials</title>
+    ...
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="ts-main-content">
+<?php include('includes/leftbar.php');?>
+<div class="content-wrapper">
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-md-12">
+        <h2 class="page-title">Manage Testimonials</h2>
+        <div class="panel panel-default">
+        <div class="panel-heading">User Testimonials</div>
+        <div class="panel-body">
+        <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+        else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+        <table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+            <thead>... </thead>
+            <tfoot>... </tfoot>
+            <tbody>
