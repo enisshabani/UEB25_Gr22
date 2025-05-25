dd64ca5641021e07951f146fa99426700a39de6a
ShqiperiMjeku1 <shqiperimjeku1@gmail.com>
2025-05-25 22:07:33 +0200

    Update index.php

--- carrental/index.php
+++ carrental/index.php
@@ -112,4 +112,128 @@ foreach($results as $result)
 </section>
 <!-- /Resent Cat --> 
 
+<!-- Fun Facts-->
+  <section class="fun-facts-section">
+    <div class="container div_zindex">
+      <div class="row">
+        <div class="col-lg-3 col-xs-6 col-sm-3">
+          <div class="fun-facts-m">
+            <div class="cell">
+              <h2><i class="fa fa-calendar" aria-hidden="true"></i>40+</h2>
+              <p>Years In Business</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6 col-sm-3">
+          <div class="fun-facts-m">
+            <div class="cell">
+              <h2><i class="fa fa-car" aria-hidden="true"></i>1200+</h2>
+              <p>New Cars For Sale</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6 col-sm-3">
+          <div class="fun-facts-m">
+            <div class="cell">
+              <h2><i class="fa fa-car" aria-hidden="true"></i>1000+</h2>
+              <p>Used Cars For Sale</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6 col-sm-3">
+          <div class="fun-facts-m">
+            <div class="cell">
+              <h2><i class="fa fa-user-circle-o" aria-hidden="true"></i>600+</h2>
+              <p>Satisfied Customers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Dark Overlay-->
+    <div class="dark-overlay"></div>
+  </section>
+  <!-- /Fun Facts-->
 
+
+  <!--Testimonial -->
+  <section class="section-padding testimonial-section parallex-bg">
+    <div class="container div_zindex">
+      <div class="section-header white-text text-center">
+        <h2>Our Satisfied <span>Customers</span></h2>
+      </div>
+      <div class="row">
+        <div id="testimonial-slider">
+          <?php
+          $tid = 1;
+          $sql = "SELECT tbltestimonial.Testimonial,tblusers.FullName from tbltestimonial join tblusers on tbltestimonial.UserEmail=tblusers.EmailId where tbltestimonial.status=:tid limit 4";
+          $query = $dbh->prepare($sql);
+          $query->bindParam(':tid', $tid, PDO::PARAM_STR);
+          $query->execute();
+          $results = $query->fetchAll(PDO::FETCH_OBJ);
+          $cnt = 1;
+          if ($query->rowCount() > 0) {
+            foreach ($results as $result) {  ?>
+
+
+              <div class="testimonial-m">
+
+                <div class="testimonial-content">
+                  <div class="testimonial-heading">
+                    <h5><?php echo htmlentities($result->FullName); ?></h5>
+                    <p><?php echo htmlentities($result->Testimonial); ?></p>
+                  </div>
+                </div>
+              </div>
+          <?php }
+          } ?>
+
+
+
+        </div>
+      </div>
+    </div>
+    <!-- Dark Overlay-->
+    <div class="dark-overlay"></div>
+  </section>
+  <!-- /Testimonial-->
+
+
+  <!--Footer -->
+  <?php include('includes/footer.php'); ?>
+  <!-- /Footer-->
+
+  <!--Back to top-->
+  <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+  <!--/Back to top-->
+
+  <!--Login-Form -->
+  <?php include('includes/login.php'); ?>
+  <!--/Login-Form -->
+
+  <!--Register-Form -->
+  <?php include('includes/registration.php'); ?>
+
+  <!--/Register-Form -->
+
+  <!--Forgot-password-Form -->
+  <?php include('includes/forgotpassword.php'); ?>
+  <!--/Forgot-password-Form -->
+
+  <!-- Scripts -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/interface.js"></script>
+  <!--Switcher-->
+  <script src="assets/switcher/js/switcher.js"></script>
+  <!--bootstrap-slider-JS-->
+  <script src="assets/js/bootstrap-slider.min.js"></script>
+  <!--Slider-JS-->
+  <script src="assets/js/slick.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:22:11 GMT -->
+
+</html>
