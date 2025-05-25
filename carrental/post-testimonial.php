1579d0265dd932787795437af09deaabc1425aae
En1sMorina <enism3828@gmail.com>
2025-05-25 19:57:45 +0200

    Update post-testimonial.php

--- carrental/post-testimonial.php
+++ carrental/post-testimonial.php
@@ -174,5 +174,20 @@ foreach($results as $result)
 
 <!--Forgot-password-Form -->
 <?php include('includes/forgotpassword.php');?>
-<!--/Forgot-password-Form --> 
+<!--/Forgot-password-Form -->
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+</html>
+<?php } ?>
 
