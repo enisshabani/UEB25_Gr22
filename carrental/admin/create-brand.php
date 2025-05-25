387a2195c65139178f4504e54c84c47915d32640
En1sMorina <enism3828@gmail.com>
2025-05-25 22:45:11 +0200

    Update create-brand.php

--- carrental/admin/create-brand.php
+++ carrental/admin/create-brand.php
@@ -1,1 +1,29 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+// Code for change password	
+if(isset($_POST['submit']))
+{
+$brand=$_POST['brand'];
+$sql="INSERT INTO  tblbrands(BrandName) VALUES(:brand)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':brand',$brand,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Brand Created successfully";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
 
+}
+?>
