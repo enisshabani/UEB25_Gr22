6eb7a1b06d317ac4418b87876f7d4a6670a07e65
piamjeku <shqiperimjeku1@gmail.com>
2025-05-25 21:17:44 +0200

    Page header and contact form

--- carrental/contact-us.php
+++ carrental/contact-us.php
@@ -89,4 +89,49 @@ if(isset($_POST['send']))
 <!--Header-->
 <?php include('includes/header.php');?>
 <!-- /Header --> 
+<section class="page-header contactus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>Contact Us</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  </div>
+  <div class="dark-overlay"></div>
+</section>
+
+<section class="contact_us section-padding">
+  <div class="container">
+    <div  class="row">
+      <div class="col-md-6">
+        <h3>Get in touch using the form below</h3>
+        <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+        else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+        <div class="contact_form gray-bg">
+          <form  method="post">
+            <div class="form-group">
+              <label class="control-label">Full Name <span>*</span></label>
+              <input type="text" name="fullname" class="form-control white_bg" id="fullname" required>
+            </div>
+            <div class="form-group">
+              <label class="control-label">Email Address <span>*</span></label>
+              <input type="email" name="email" class="form-control white_bg" id="emailaddress" required>
+            </div>
+            <div class="form-group">
+              <label class="control-label">Phone Number <span>*</span></label>
+              <input type="text" name="contactno" class="form-control white_bg" id="phonenumber" required maxlength="10" pattern="[0-9]+">
+            </div>
+            <div class="form-group">
+              <label class="control-label">Message <span>*</span></label>
+              <textarea class="form-control white_bg" name="message" rows="4" required></textarea>
+            </div>
+            <div class="form-group">
+              <button class="btn" type="submit" name="send" type="submit">Send Message <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+            </div>
+          </form>
+        </div>
 
