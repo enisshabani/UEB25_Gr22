4758599f0869679d9c0eb24da81e1d41d076bc3f
En1sMorina <enism3828@gmail.com>
2025-05-25 20:02:44 +0200

    Update profile.php

--- carrental/profile.php
+++ carrental/profile.php
@@ -1,1 +1,32 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+  { 
+header('location:index.php');
+}
+else{
+if(isset($_POST['updateprofile']))
+  {
+$name=$_POST['fullname'];
+$mobileno=$_POST['mobilenumber'];
+$dob=$_POST['dob'];
+$adress=$_POST['address'];
+$city=$_POST['city'];
+$country=$_POST['country'];
+$email=$_SESSION['login'];
+$sql="update tblusers set FullName=:name,ContactNo=:mobileno,dob=:dob,Address=:adress,City=:city,Country=:country where EmailId=:email";
+$query = $dbh->prepare($sql);
+$query->bindParam(':name',$name,PDO::PARAM_STR);
+$query->bindParam(':mobileno',$mobileno,PDO::PARAM_STR);
+$query->bindParam(':dob',$dob,PDO::PARAM_STR);
+$query->bindParam(':adress',$adress,PDO::PARAM_STR);
+$query->bindParam(':city',$city,PDO::PARAM_STR);
+$query->bindParam(':country',$country,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->execute();
+$msg="Profile Updated Successfully";
+}
 
+?>
