829771993d30b20f097e2591d653405dda522d34
En1sMorina <enism3828@gmail.com>
2025-05-25 19:54:48 +0200

    Update post-testimonial.php

--- carrental/post-testimonial.php
+++ carrental/post-testimonial.php
@@ -118,4 +118,16 @@ if($query->rowCount() > 0)
 {
 foreach($results as $result)
 { ?>
+<section class="user_profile inner_pages">
+  <div class="container">
+    <div class="user_profile_info gray-bg padding_4x4_40">
+      <div class="upload_user_logo"> <img src="assets/images/dealer-logo.jpg" alt="image">
+      </div>
+
+      <div class="dealer_info">
+        <h5><?php echo htmlentities($result->FullName);?></h5>
+        <p><?php echo htmlentities($result->Address);?><br>
+          <?php echo htmlentities($result->City);?>&nbsp;<?php echo htmlentities($result->Country); }}?></p>
+      </div>
+    </div>
 
