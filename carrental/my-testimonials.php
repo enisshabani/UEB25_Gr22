2c667e33884831ac8bc146238062c74e32a2c46d
lispahijaa <118270737+lispahijaa@users.noreply.github.com>
2025-05-25 22:18:07 +0200

    Update my-testimonials

--- carrental/my-testimonials.php
+++ carrental/my-testimonials.php
@@ -67,3 +67,104 @@ else{
   <div class="dark-overlay"></div>
 </section>
 <!-- /Page Header--> 
+
+<?php 
+$useremail=$_SESSION['login'];
+$sql = "SELECT * from tblusers where EmailId=:useremail";
+$query = $dbh -> prepare($sql);
+$query -> bindParam(':useremail',$useremail, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+<section class="user_profile inner_pages">
+  <div class="container">
+    <div class="user_profile_info gray-bg padding_4x4_40">
+      <div class="upload_user_logo"> <img src="assets/images/dealer-logo.jpg" alt="image">
+      </div>
+
+      <div class="dealer_info">
+        <h5><?php echo htmlentities($result->FullName);?></h5>
+        <p><?php echo htmlentities($result->Address);?><br>
+          <?php echo htmlentities($result->City);?>&nbsp;<?php echo htmlentities($result->Country); }}?></p>
+      </div>
+    </div>
+  
+  <div class="row">
+      <div class="col-md-3 col-sm-3">
+        <?php include('includes/sidebar.php');?>
+      <div class="col-md-8 col-sm-8">
+
+
+
+        <div class="profile_wrap">
+          <h5 class="uppercase underline">My Testimonials </h5>
+          <div class="my_vehicles_list">
+            <ul class="vehicle_listing">
+<?php 
+$useremail=$_SESSION['login'];
+$sql = "SELECT * from tbltestimonial where UserEmail=:useremail";
+$query = $dbh -> prepare($sql);
+$query -> bindParam(':useremail',$useremail, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+if($cnt=$query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+
+              <li>
+           
+                <div>
+                 <p><?php echo htmlentities($result->Testimonial);?> </p>
+                   <p><b>Posting Date:</b><?php echo htmlentities($result->PostingDate);?> </p>
+                </div>
+                <?php if($result->status==1){ ?>
+                 <div class="vehicle_status"> <a class="btn outline btn-xs active-btn">Active</a>
+
+                  <div class="clearfix"></div>
+                  </div>
+                  <?php } else {?>
+               <div class="vehicle_status"> <a href="#" class="btn outline btn-xs">Waiting for approval</a>
+                  <div class="clearfix"></div>
+                  </div>
+                  <?php } ?>
+              </li>
+              <?php } } ?>
+              
+            </ul>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/my-vehicles--> 
+
+<<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+</html>
+<?php } ?>
