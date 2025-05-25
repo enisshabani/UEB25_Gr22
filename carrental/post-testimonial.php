0fa1fab3e050ea2318d28e575d394e2556933db2
En1sMorina <enism3828@gmail.com>
2025-05-25 19:50:40 +0200

    Update post-testimonial.php

--- carrental/post-testimonial.php
+++ carrental/post-testimonial.php
@@ -1,1 +1,31 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+  { 
+header('location:index.php');
+}
+else{
+if(isset($_POST['submit']))
+  {
+$testimonoial=$_POST['testimonial'];
+$email=$_SESSION['login'];
+$sql="INSERT INTO  tbltestimonial(UserEmail,Testimonial) VALUES(:email,:testimonoial)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':testimonoial',$testimonoial,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
 
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Testimonail submitted successfully";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+?>
