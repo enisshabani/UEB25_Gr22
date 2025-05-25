4ae30b8ecc6a1d0496f94a11b2e6c4f326b8f218
En1sMorina <enism3828@gmail.com>
2025-05-25 23:17:52 +0200

    Update reg-users.php

--- carrental/admin/reg-users.php
+++ carrental/admin/reg-users.php
@@ -68,3 +68,64 @@ $msg="Page data updated  successfully";
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
 }
 		</style>
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+
+	<div class="ts-main-content">
+		<?php include('includes/leftbar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+
+						<h2 class="page-title">Registered Users</h2>
+
+						<!-- Zero Configuration Table -->
+						<div class="panel panel-default">
+							<div class="panel-heading">Reg Users</div>
+							<div class="panel-body">
+							<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+								<table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+									<thead>
+										<tr>
+										<th>#</th>
+												<th> Name</th>
+											<th>Email </th>
+											<th>Contact no</th>
+										<th>DOB</th>
+										<th>Address</th>
+										<th>City</th>
+										<th>Country</th>
+										<th>Reg Date</th>
+										
+										</tr>
+									</thead>
+									<tfoot>
+										<tr>
+										<th>#</th>
+											<th> Name</th>
+											<th>Email </th>
+											<th>Contact no</th>
+										<th>DOB</th>
+										<th>Address</th>
+										<th>City</th>
+										<th>Country</th>
+										<th>Reg Date</th>
+										</tr>
+										</tr>
+									</tfoot>
+									<tbody>
+
+									<?php $sql = "SELECT * from  tblusers ";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{				?>	
