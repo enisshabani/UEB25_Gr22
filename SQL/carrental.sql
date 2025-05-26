407442fb6f46d543f64c51d88fc90f1abc27dd5a
RinesaS1 <rinesaselmonaj4@gmail.com>
2025-05-26 14:41:16 +0200

    Added admin-related tables: admin, tblusers, tblsubscribers, tbltestimonial

---
+++ SQL/carrental.sql
@@ -0,0 +1,72 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Table structure for table `admin`
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table `admin`
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '5c428d8875d2948607f3e3fe134d71b4', '2024-05-01 12:22:38');
+
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `EmailId` varchar(100) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `ContactNo` char(11) DEFAULT NULL,
+  `dob` varchar(100) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(100) DEFAULT NULL,
+  `Country` varchar(100) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `FullName`, `EmailId`, `Password`, `ContactNo`, `dob`, `Address`, `City`, `Country`, `RegDate`, `UpdationDate`) VALUES
+(1, 'Test', 'test@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '6465465465', '', 'L-890, Gaur City Ghaziabad', 'Ghaziabad', 'India', '2024-05-01 14:00:49', '2024-06-05 05:27:37'),
+(2, 'Amit', 'amikt12@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '1425365214', NULL, NULL, NULL, NULL, '2024-06-05 05:31:05', NULL);
+
+-- 
+-- Table structure for table `tblsubscribers`
+--
+
+CREATE TABLE `tblsubscribers` (
+  `id` int(11) NOT NULL,
+  `SubscriberEmail` varchar(120) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblsubscribers`
+--
+
+INSERT INTO `tblsubscribers` (`id`, `SubscriberEmail`, `PostingDate`) VALUES
+(4, 'harish@gmail.com', '2024-06-01 09:26:21'),
+(5, 'kunal@gmail.com', '2024-05-31 09:35:07');
+
+--
+-- Table structure for table `tbltestimonial`
+--
+
+CREATE TABLE `tbltestimonial` (
+  `id` int(11) NOT NULL,
+  `UserEmail` varchar(100) NOT NULL,
+  `Testimonial` mediumtext NOT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
